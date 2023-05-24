1b38e83ced03cf7a220e481768c5f9e524a57c09
Ivy Mendoza <ivy.mendoza@rise-x.io>
2023-05-24 11:31:43 +0800

    Add project files.

---
+++ VindicateCo/index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="./css/style.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" > </script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+    <title>Vindicate Co.</title>
+  </head>
+  <body>
+  <header>
+            
+            <div class="text-center" id="vindicate">Find Your Look
+                             <!--FIRST NAV-->
+                            <div class= "border-bottom border-light" style="background-color:#fdfdf9; z-index: 99999;" >
+                                   <!-- <div id="top-bar" class="top-navi">
+                                        <ul class="nav justify-content-end">
+                                            <li class="nav-item"><a href="#"><img class="cart" alt="Cart" src="images/cart.png"></a></li>
+                                            <li class="nav-item active"><a class="nav-link text-dark" href="reg.php">Sign Up</a></li>
+                                            <li class="nav-item active"><a class="nav-link text-dark" href="login.php">Sign In</a></li>
+                                        </ul>
+                                    
+                                    </div>      
+                                    -->
+
+                                <!--LOGO 
+                                    <div class="text-center">
+                                        <a href="index.php"><img class="logo" alt="Logo" src="images/logo.png" /></a>
+                                    </div>
+                    -->
+
+                                <!--NAVIGATION-->    
+                        
+                                <div class="main-navi">
+                                    
+                                        <ul class="nav justify-content-center">
+                                        <li class="nav-item"><a class="nav-link text-dark" href="products.php"><b>SHIRTS</b></a></li>
+                                        <li class="nav-item"><a class="nav-link text-dark" href="custom.php"><b>CUSTOM</b></a></li>
+                                        <li class="nav-item"><a class="nav-link text-dark" href="#"><b>ABOUT</b></a></li>
+                                    </ul>
+                                </div>
+                        </div>
+                </div>
+            </header>
+<!--CAROUSEL-->
+<div class="container"></div>
+             
+             <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="3000" data-pause="hover" style="margin-top: 54px;">
+      <!-- Indicators -->
+               <ol class="carousel-indicators">
+                 <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                 <li data-target="#myCarousel" data-slide-to="1"></li>
+                 <li data-target="#myCarousel" data-slide-to="2"></li>
+                 
+               </ol>
+           
+      <!-- Wrapper for slides -->
+               <div class="carousel-inner">
+                 <div class="item active" >
+                   <img src="images/045.jpg" alt="Couch" style="width:100%; height: 700px;">
+                 </div>
+           
+                 <div class="item">
+                   <img src="images/046.jpg" alt="Dining" style="width:100%; height: 700px;">
+                 </div>
+               
+                 <div class="item">
+                   <img src="images/047.jpg" alt="Door" style="width:100%; height: 700px;">
+                 </div>
+               </div>
+          
+      <!-- Left and right controls -->
+               <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                 <span class="glyphicon glyphicon-chevron-left"></span>
+                 <span class="sr-only">Previous</span>
+               </a>
+               <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                 <span class="glyphicon glyphicon-chevron-right"></span>
+                 <span class="sr-only">Next</span>
+               </a>
+             </div>
+           </div>
+         </div>
+
+         <div class="text-center" style="font-family: 'Zona Pro Light'; font-size: 25px; margin-top: 60px;">
+                  <b>TELL US WHAT YOU WANT & WE WILL BUILD IT FOR YOU!</b><br /><br /><br />
+                  Specializing in the design and build of furniture in residential homes and<br />commercial spaces,
+                       that ensure quality and sophistication<br />for every renovation work and project.
+                      </div>
+                
+              
+                  <div class="text-center"><img src="images/step.png" style="margin-top: 60px;"/></div>
+          
+                
+                
+                 
+                  
+                  
+
+
+<!--FEATURED PRODUCTS-->    
+
+                  
+
+
+            <!--FOOTER-->
+            <hr/>
+            <footer>
+              <div class="container">
+                <p class="pull-right"><a href="#">Back to Top</a></p>
+                <p>&copy; 2018 MCDO FURNITURE 
+                  &middot; <a href="index.php">HOME</a> 
+                  &middot; <a href="products.php">FURNITURE</a>
+                  &middot; <a href="customize.php">CUSTOMIZE</a>
+                  &middot; <a href="#">ABOUT</a>
+                  &middot; <a href="reg.php">SIGN UP</a>
+                  &middot; <a href="login.php">SIGN IN</a>
+                </p>
+              </div>
+          </footer>
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
